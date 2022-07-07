63cd6624b04635b6ce30bb0c2b93f89233bae934
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-07-07 12:26:06 -0500

    Clean code, deleting some unnecesary lines and DRY

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,13 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: %i[ show edit update destroy ]
+  before_action :set_book, only: %i[show edit update destroy]
 
-  # GET /books or /books.json
+  # GET /books
   def index
     @books = Book.all
   end
 
-  # GET /books/1 or /books/1.json
-  def show
-  end
+  # GET /books/1
+  def show; end
 
   # GET /books/new
   def new
@@ -16,56 +15,50 @@ class BooksController < ApplicationController
   end
 
   # GET /books/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /books or /books.json
+  # POST /books
   def create
     @book = Book.new(book_params)
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to books_url, notice: "Saved!" }
-        format.json { render :show, status: :created, location: @book }
+        format.html { redirect_to books_url, notice: 'Saved!' }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /books/1 or /books/1.json
+  # PATCH/PUT /books/1
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to books_url, notice: "Updated!" }
-        format.json { render :show, status: :ok, location: @book }
+        format.html { redirect_to books_url, notice: 'Updated!' }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /books/1 or /books/1.json
+  # DELETE /books/1
   def destroy
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book deleted!" }
-      format.json { head :no_content }
+      format.html { redirect_to books_url, notice: 'Book deleted!' }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def book_params
-      params.require(:book).permit(:name, :author)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = Book.find(params[:id])
+  end
 
+  # Only allow a list of trusted parameters through.
+  def book_params
+    params.require(:book).permit(:name, :author)
+  end
 end
